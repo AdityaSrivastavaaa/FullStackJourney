14714f36b5382723e8acbc44f5e70cd94e3c96af
Aditya Srivastava <srivastavaaditya015@gmail.com>
2026-02-25 15:30:19 +0530

    completed react router implementation, added new components and updated existing ones to enhance the functionality and user experience of the application. The changes include modifications to the package files, addition of new components for better navigation, and updates to the main application file to integrate the new features seamlessly.

--- React/07reactRouter/src/App.jsx
+++ React/07reactRouter/src/App.jsx
@@ -1,11 +1,15 @@
-
+import Home from "./components/Home/Home"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
 
 function App() {
   
 
   return (
     <>
-    
+      <Header/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
