de4cdfafe9a06085992a0dfc5bece3286a14d1d2
Aditya Srivastava <srivastavaaditya015@gmail.com>
2026-02-26 11:02:19 +0530

    completed context with local storage and todo app

---
+++ React/10todoproject/src/context/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext,useContext } from "react";
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo Message",
+            completed:false,
+        }
+    ],
+    addTodo:(todo)=>{},
+    toggleComplete:(id)=>{},
+    deleteTodo:(id)=>{},
+    updateTodo:(id,todo)=>{},
+});
+
+export const useTodo =()=>{
+    return useContext(TodoContext);
+}
+
+export const TodoProvider = TodoContext.Provider;
